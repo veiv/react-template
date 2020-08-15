1ffa71ea2ecd3ecae3c494bae4abd2a78db4795c
Vladimir Fomenko <vlfomenk@gmail.com>
2020-08-15 22:30:59 +0200

    Configure FB deployment based on branch

--- src/components/App/__test__/App.test.js
+++ src/components/App/__test__/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "components/App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+    const { getAllByText } = render(<App />);
+    const linkElement = getAllByText(/Hello world/i);
+    expect(linkElement.length).toBe(2);
 });
